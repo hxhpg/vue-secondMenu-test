70ca29573c9321cd1cbacdac698855de8b01650a
hxhpg <75286808+hxhpg@users.noreply.github.com>
2021-04-29 10:04:21 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,32 +1,34 @@
-import axios from "axios";
-import global from '../global/global'
+import axios from 'axios';
+import global from '../global/global';
 
 axios.defaults.baseURL = global.baseURL;
-var http = {
-  request({ url, data = {}, method = "GET" }) {
-    return new Promise((resolve, reject) => {
-      this._request(url, resolve, reject, data, method);
-    });
-  },
-  _request: function(url, resolve, reject, data = {}, method = "GET") {
-    let format = method.toLocaleLowerCase() ==='get'?'params':'data';
-    axios({
-      url:url,
-      method: method,
-      [format]: data,
-      header: {
-        "content-type": "application/json"
-      }
-    }).then(res => {
-      if (res.status == 200) {
-        resolve(res.data);
-      } else if (res.code == -1) {
-        resolve(res);
-      }
-    }).catch(() => {
-      reject();
- 
-    })
-  }
-};
+
+const http = {
+    request ({ url, data = {}, method = 'GET', responseType }) {
+        return new Promise((resolve, reject) => {
+            this._request(url, resolve, reject, data, method, responseType);
+        });
+    },
+    _request (url, resolve, reject, data = {}, method = 'GET', responseType) {
+        const format = method.toLocaleLowerCase() == 'get' ? 'params' : 'data';
+        axios({
+            url: url,
+            method: method,
+            [format]: data,
+            header: {
+                'content-type': 'application/json'
+            },
+            responseType
+        }).then(res => {
+                if (res.status == 200) {
+                    resolve(res.data);
+                } else if (res.code == -1) {
+                    resolve(res);
+                }
+            }).catch(error => {
+                reject(error);
+            })
+    }
+}
+
 export { http };
